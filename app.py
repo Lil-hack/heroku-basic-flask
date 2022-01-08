58f1986ecb6d4ec5cb497a0a5bfd019607a69b39
LilHack <43441041+Lil-hack@users.noreply.github.com>
2022-01-08 21:53:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,12 @@ from selenium import webdriver
 app = Flask(__name__)
 import os
 import random
-
+from threading import Thread, Lock
 from flask import send_file
 
-@app.route('/get_image')
-def get_image():
-    filename = 'ip.png'
-    return send_file(filename, mimetype='image/png')
 
-@app.route('/')
-def homepage():
+url="https://www.twitch.tv/satomin2007"
+def new():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
@@ -22,6 +18,25 @@ def homepage():
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get("http://check.torproject.org")
     driver.save_screenshot(f'ip.png')
+    driver.get(url)
+
+        for i in range(0, 100):
+            wd.save_screenshot(f'photo.png')
+            print('lox2')
+            time.sleep(500)
+            wd.get(url)
+    
+@app.route('/get_image')
+def get_image():
+    filename = 'ip.png'
+    return send_file(filename, mimetype='image/png')
+@app.route('/get_image2')
+def get_image():
+    filename = 'photo.png'
+    return send_file(filename, mimetype='image/png')
+@app.route('/')
+def homepage():
+    Thread(target=new).start()
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
