dfe71c8b09f50cde012d7215346570497666bfe8
LilHack <43441041+Lil-hack@users.noreply.github.com>
2022-01-08 21:28:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
 from datetime import datetime
+from selenium import webdriver
 app = Flask(__name__)
+import os
+
+
 
 @app.route('/')
 def homepage():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
