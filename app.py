e8efbfbae470363ef3fd2590854fc9d9af2a914a
LilHack <43441041+Lil-hack@users.noreply.github.com>
2022-01-10 18:56:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,33 +29,37 @@ def new(url):
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get(url)
     driver.save_screenshot(f'ip.png')
-    urlcockie='https://www.twitch.tv/'
-    file1 = open('cockiestwitch/' + list_cockie[random.randint(1,len(list_cockie)-1)], 'r')
-    for item in file1:
-        data = item.split('	')
-        if urlcockie.find(data[0]) != -1:
-            print(file1)
-            value = data[6].replace(' ', '').replace('\r', '').replace('\n', '')
-            cookie_dict = {
-                'name': data[5],
-                'value': value,
-                "domain": data[0],  # google chrome
-                "expires": data[4],
-                'path': data[2],
-                'httpOnly': False,
-                'HostOnly': False,
-                'Secure': False
-            }
-            driver.add_cookie(cookie_dict)
+    try:
+        urlcockie='https://www.twitch.tv/'
+        file1 = open('cockiestwitch/' + list_cockie[random.randint(1,len(list_cockie)-1)], 'r')
+        for item in file1:
+            data = item.split('	')
+            if urlcockie.find(data[0]) != -1:
+                print(file1)
+                value = data[6].replace(' ', '').replace('\r', '').replace('\n', '')
+                cookie_dict = {
+                    'name': data[5],
+                    'value': value,
+                    "domain": data[0],  # google chrome
+                    "expires": data[4],
+                    'path': data[2],
+                    'httpOnly': False,
+                    'HostOnly': False,
+                    'Secure': False
+                }
+                driver.add_cookie(cookie_dict)
+    except Exception as e:
+        pass
     driver.get(url)
     
     for i in range(0, 100):
-        driver.save_screenshot(f'photo.png')
+        
         time.sleep(5)
         try:
             driver.find_element_by_xpath('//*[@id="root"]/div/div[2]/div[1]/main/div[2]/div[3]/div/div/div[2]/div/div[2]/div/div/div/div/div[7]/div/div[3]/button').click()
         except Exception as e:
             pass
+        driver.save_screenshot(f'photo.png')
         print('lox2')
         time.sleep(500)
         driver.get(url)
