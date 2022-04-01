d304234c63eb3090a008587e98d7ae0b15f62f33
LilHack <43441041+Lil-hack@users.noreply.github.com>
2022-04-02 00:53:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import random
 from threading import Thread, Lock
 from flask import send_file
 import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
         
