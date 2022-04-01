02759b74dc1449e67109b2c8cd7543dffb6f0a49
LilHack <43441041+Lil-hack@users.noreply.github.com>
2022-04-02 00:31:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -43,40 +43,28 @@ def new(url):
         
 def new_youtube(url):
     print(url)
-    url=f'https://www.youtube.com/watch?v={url}'
+    url=f'https://www.youtube.com'
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    driver.get("http://check.torproject.org")
-    driver.save_screenshot(f'ip.png')
     driver.get(url)
-    file1 = open('cockiestwitch/' + list_cockie[random.randint(1,len(list_cockie))], 'r')
-    driver.delete_all_cookies()
-    for item in file1:
-        data = item.split('	')
-        if url.find(data[0]) != -1:
-            print(file1)
-            value = data[6].replace(' ', '').replace('\r', '').replace('\n', '')
-            cookie_dict = {
-                'name': data[5],
-                'value': value,
-                "domain": data[0],  # google chrome
-                "expires": data[4],
-                'path': data[2],
-                'httpOnly': False,
-                'HostOnly': False,
-                'Secure': False
-            }
-            driver.add_cookie(cookie_dict)
-    driver.get(url)
+    stroka='Твое имя наше последнее лето'
     time.sleep(5)
-    video = driver.find_element_by_id('movie_player')
-    video.send_keys(Keys.SPACE) #hits space
-    time.sleep(1)
-    video.click()   
+    try:
+            driver.find_element_by_xpath('/html/body/ytd-app/div[1]/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').send_keys(stroka)
+            driver.find_element_by_xpath('/html/body/ytd-app/div[1]/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input').send_keys(Keys.ENTER)
+    except Exception as e:
+            print(e)
+    time.sleep(4)
+    try:
+            driver.find_element_by_xpath('/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[2]/div[1]/ytd-thumbnail/a/yt-img-shadow/img').click()
+            time.sleep(2)
+    except Exception as e:
+            print(e)
+          
     for i in range(0, 100):
         driver.save_screenshot(f'photo.png')
         print('lox2')
@@ -93,7 +81,7 @@ def get_image2():
     filename = 'photo.png'
     return send_file(filename, mimetype='image/png')
 
-@app.route('/youtube/<url>')
+@app.route('/youtube')
 def homepage_youtube(url):
     print(url)
     Thread(target=new_youtube,args=([url])).start()
