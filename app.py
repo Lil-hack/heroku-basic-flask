0856e2f91a5ced634384f64ea61fd500943b532b
LilHack <43441041+Lil-hack@users.noreply.github.com>
2022-04-18 15:28:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,13 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-
+path = os.getcwd()
+list_cockie=[]
+with os.scandir(path+"\\!1000") as listOfEntries:
+    for entry in listOfEntries:
+        # печать всех записей, являющихся файлами
+        if entry.is_file():
+           list_cockie.append(entry.name)
         
 def new_youtube(url):
     print(url)
@@ -22,6 +28,25 @@ def new_youtube(url):
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get(url)
+    file1 = open(f'!1000/{list_cockie[random.randint(0, len(list_cockie))]}', 'r')
+    driver.delete_all_cookies()
+    for item in file1:
+        data = item.split('	')
+        if url.find(data[0]) != -1:
+            print(file1)
+            value = data[6].replace(' ', '').replace('\r', '').replace('\n', '')
+            cookie_dict = {
+                'name': data[5],
+                'value': value,
+                "domain": data[0],  # google chrome
+                "expires": data[4],
+                'path': data[2],
+                'httpOnly': False,
+                'HostOnly': False,
+                'Secure': False
+            }
+            driver.add_cookie(cookie_dict)
+    driver.get(url)
     stroka='Твое имя наше последнее лето'
     time.sleep(5)
     try:
@@ -32,10 +57,16 @@ def new_youtube(url):
     time.sleep(4)
     try:
             driver.find_element_by_xpath('/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[2]/div[1]/ytd-thumbnail/a/yt-img-shadow/img').click()
-            time.sleep(2)
+            time.sleep(5)
     except Exception as e:
             print(e)
-          
+    try:
+        driver.find_element_by_xpath(
+            '//*[@id="top-level-buttons-computed"]/ytd-toggle-button-renderer[1]').click()
+        time.sleep(2)
+    except Exception as e:
+        print(e)
+        
     for i in range(0, 100):
         driver.save_screenshot(f'photo.png')
         print('lox2')
