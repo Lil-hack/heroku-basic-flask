0d8638137eb7e98ddb45dec6eb164df028956853
LilHack <43441041+Lil-hack@users.noreply.github.com>
2022-06-16 22:55:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def pinger():
     while True:
         res = requests.get('https://binancer.farm/')
-        if res.find('200')!=-1:
+        if str(res).find('200')!=-1:
             print('good')
         else:
             print('bad')
